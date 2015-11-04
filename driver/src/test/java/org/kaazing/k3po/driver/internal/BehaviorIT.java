1505701a8eca16dd451d505582d5620e8b2df57c
Chris Barrow <chris.barrow@kaazing.com>
2015-11-04 13:31:30 -0800

    Moved test case for k3po#222 from BehaviorIT into a new test TcpIT, and added a second test case server.write.flush.closed.rpt which shows a similar issue on the server (accept) side.

--- driver/src/test/java/org/kaazing/k3po/driver/internal/BehaviorIT.java
+++ driver/src/test/java/org/kaazing/k3po/driver/internal/BehaviorIT.java
@@ -19,7 +19,6 @@ package org.kaazing.k3po.driver.internal;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
@@ -101,12 +100,6 @@ public class BehaviorIT {
     public void testPassingBarriers() throws Exception {
         k3po.notifyBarrier("AWAITING_BARRIER");
         k3po.awaitBarrier("NOTIFYING_BARRIER");
-        k3po.finish();
-    }
-
-    @TestSpecification("closed.vs.disconnected.issue")
-    @Test
-    public void closeVsDisconnectedIssue() throws Exception {
         k3po.finish();
     }
 }
